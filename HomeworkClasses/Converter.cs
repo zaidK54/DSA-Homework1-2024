7dc6b06fd0e1037299ebfec0e16c964c036ea819
Huda <huda.f.shakir@gmail.com>
2024-11-01 14:09:37 +0300

    homework ready

--- HomeworkClasses/Converter.cs
+++ HomeworkClasses/Converter.cs
@@ -10,15 +10,8 @@ namespace DSA_Homework1.HomeworkClasses
     {
         public string DecimalToBinary(int decimalNumber)
         {
-            //// TODO: convert decimal number to binary code using recursion
-            //throw new NotImplementedException();
-
-            // Base case: if number is 0, return an empty string
-            if (decimalNumber == 0)
-                return "";
-
-            // Recursive call to get the binary representation of the quotient
-            return DecimalToBinary(decimalNumber / 2) + (decimalNumber % 2).ToString();
+            // TODO: convert decimal number to binary code using recursion
+            throw new NotImplementedException();
         }
     }
 }
