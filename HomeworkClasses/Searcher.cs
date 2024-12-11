b07debf6cd7b449585a1737c50ab64e96d598582
zaidK54 <169089642+zaidK54@users.noreply.github.com>
2024-12-11 20:03:14 +0300

    Update Searcher.cs

--- HomeworkClasses/Searcher.cs
+++ HomeworkClasses/Searcher.cs
@@ -11,7 +11,17 @@ namespace DSA_Homework1
         public int TernarySearch(int[] array, int target, int left, int right)
         {
             // TODO: Implement recursive ternary search
-            throw new NotImplementedException();
+            int m1 = left + (right - left) / 2;
+            int m2 = right - (right - left) / 2;
+            if (array[m1] == target) return m1;
+            if (array[m2] == target) return m2;
+            if (left == right || right - left == 1) return -1;
+
+            if (array[m1] > target) right = m1;
+            else left = m1;
+
+            if (array[m2] < target) left = m2;
+            return TernarySearch(array, target, left, right);
         }
     }
 }
