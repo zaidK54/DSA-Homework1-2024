c0af8f58015ad858d32bca3a3de84ada503f9565
zaidK54 <169089642+zaidK54@users.noreply.github.com>
2024-12-16 19:28:49 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -20,15 +20,17 @@ namespace DSA_Homework1
 
 
             var searcher = new Searcher();
+            Console.Write("Our array :\t");
             int[] array = { 1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21 };
-            Console.Write("Searching array:\t");
             foreach (var item in array)
             {
-                Console.Write(item);
+                Console.Write(item + " ");
             }
             Console.WriteLine();
-            int result = searcher.TernarySearch(array, 1, 0, array.Length);
-            Console.WriteLine("Element Found at "+result);
+            Console.Write("Searching array:\t");
+            int.TryParse(Console.ReadLine(), out num);
+            int result = searcher.TernarySearch(array, num, 0, array.Length);
+            Console.WriteLine("Element Found at " + result);
         }
     }
 }
